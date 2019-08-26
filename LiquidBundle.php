5a15df03405f9675b93660619c538e05e3b0bf94
Andrey <firstkb@gmail.com>
2019-08-26 18:25:49 +0300

    start bundle

---
+++ LiquidBundle.php
@@ -0,0 +1,12 @@
+<?php
+namespace Symfony\Bundle\LiquidBundle;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LiquidBundle extends Bundle
+{
+
+}
