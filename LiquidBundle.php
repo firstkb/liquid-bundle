f65c9d670081a0e14c6cc21914b605835bec8ff1
Andrey <firstkb@gmail.com>
2019-08-26 18:56:44 +0300

    update composer.json

--- LiquidBundle.php
+++ LiquidBundle.php
@@ -1,9 +1,6 @@
 <?php
 namespace Symfony\Bundle\LiquidBundle;
 
-use Symfony\Component\Console\Application;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LiquidBundle extends Bundle
